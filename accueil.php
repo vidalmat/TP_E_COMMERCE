38560ba764fba6ac8af83a5d7ee065fc7c0d81d5
vidalmat <72913872+vidalmat@users.noreply.github.com>
2021-01-23 11:58:37 +0100

    Add files via upload

---
+++ accueil.php
@@ -0,0 +1,14 @@
+<?php
+require_once "articles.php";
+
+
+?>      
+        <h1 id="titre_accueil">Bienvenue dans le monde des jeux vidéos</h1>
+
+        <img src="images/fond_accueil.jpg" alt="fond page accueil" id="fond_accueil">
+
+        
+        
+    </body>
+
+</html>
